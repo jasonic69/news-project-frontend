563d042e0683f596476cb22cf3009db3b8a6ec62
jasonic69 <justyhill3@hotmail.com>
2024-09-17 16:20:05 +0100

    task5-View-an-individual-article

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
-import ArticlesContainer from "./components/ArticlesContainer"
+import { Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import Home from './components/Home'
 import Navigation from './components/Navigation'
+import SingleArticle from './components/SingleArticle'
+import Error404 from './components/404'
 
 function App() {
 
@@ -9,7 +12,11 @@ function App() {
     <>
     <Header/>
     <main>
-    <ArticlesContainer/>
+      <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/article/:id" element={<SingleArticle />}/>
+          <Route path="/404" element={<Error404 />}/>
+      </Routes>
     </main>
     <Navigation/>
     </>
